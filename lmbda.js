ba13888ef8e7ad8f7fd0d21d2b13211cf3eddafb
atangger <twjjwt@pku.edu.cn>
2018-10-17 19:58:07 -0400

    add the lumbda

---
+++ lmbda.js
@@ -0,0 +1,82 @@
+'use strict';
+console.log('Loading hello world function');
+ 
+exports.handler = function(event, context, callback) {
+    let name = "you";
+    let city = 'World';
+    let time = 'day';
+    let day = '';
+    let responseCode = 200;
+    let mymessage = '';
+    console.log("request: " + JSON.stringify(event));
+    
+    // This is a simple illustration of app-specific logic to return the response. 
+    // Although only 'event.queryStringParameters' are used here, other request data, 
+    // such as 'event.headers', 'event.pathParameters', 'event.body', 'event.stageVariables', 
+    // and 'event.requestContext' can be used to determine what response to return. 
+    //
+    if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) {
+        if (event.queryStringParameters.name !== undefined && 
+            event.queryStringParameters.name !== null && 
+            event.queryStringParameters.name !== "") {
+            console.log("Received name: " + event.queryStringParameters.name);
+            name = event.queryStringParameters.name;
+        }
+    }
+    
+    if (event.pathParameters !== null && event.pathParameters !== undefined) {
+        if (event.pathParameters.proxy !== undefined && 
+            event.pathParameters.proxy !== null && 
+            event.pathParameters.proxy !== "") {
+            console.log("Received proxy: " + event.pathParameters.proxy);
+            city = event.pathParameters.proxy;
+        }
+    }
+    
+    if (event.headers !== null && event.headers !== undefined) {
+        if (event.headers['day'] !== undefined && event.headers['day'] !== null && event.headers['day'] !== "") {
+            console.log("Received day: " + event.headers.day);
+            day = event.headers.day;
+        }
+    }
+    
+    if (event.body !== null && event.body !== undefined) {
+        let body = JSON.parse(event.body)
+        if (body.time) 
+            time = body.time;
+        if (body.name2)
+            name = body.name2;
+        if (body.city)
+            city = body.city;
+        if (body.message)
+            mymessage = body.message;
+    }
+ 
+    let greeting = 'Good ' + time + ', ' + name + ' of ' + city + '. ';
+    if (day) greeting += 'Happy ' + day + '!';
+    greeting += 'you said : ' + mymessage;
+    var responseBody = {
+        message: greeting,
+        input: event
+    };
+    
+    // The output from a Lambda proxy integration must be 
+    // of the following JSON object. The 'headers' property 
+    // is for custom response headers in addition to standard 
+    // ones. The 'body' property  must be a JSON string. For 
+    // base64-encoded payload, you must also set the 'isBase64Encoded'
+    // property to 'true'.
+    var response = {
+        statusCode: responseCode,
+        headers: {
+            "x-custom-header" : "my custom header value",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST,GET",
+            
+            "Access-Control-Allow-Headers":"x-api-key"
+        },
+        body: JSON.stringify(responseBody)
+    };
+    console.log("response: " + JSON.stringify(response))
+    callback(null, response);
+};
